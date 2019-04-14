7aeb87f046b7f74ccf65822288d92595d25205aa
saheb222 <sseikhmy@gmail.com>
2019-04-14 09:27:32 +0530

    edge case in binary serach

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,5 @@
 def bin_search(a,n,min,max):
-	if min>=max:
+	if min>max:
 		return False
 	else:
 		mid = (min+max)//2
@@ -9,7 +9,7 @@ def bin_search(a,n,min,max):
 			return bin_search(a,n,mid+1,max)
 		else :
 			return bin_search(a,n,min,mid-1)
-a= [2,3,4,5,6,7,8,10,14,15,18,19,20,24,27,29,30,40,50]
-n=int(input("please enter the searcing element\n"))
+a= [2,3,4,5,6,7,8,9,11,12,14]
+n=int(input("please enter the element to be searched\n"))
 result = bin_search(a,n,0,len(a)-1)
 print(result)
